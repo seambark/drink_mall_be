910171ecf5e4b5a7c9b5104fc38471f36aaa45f2
Seam Park <run128@naver.com>
2024-06-01 23:17:39 +0900

    login

---
+++ controllers/auth.controller.js
@@ -0,0 +1,26 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+
+const authController = {};
+
+authController.loginWithEmail = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    let user = await User.findOne({ email });
+
+    if (user) {
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (isMatch) {
+        const token = await user.generateToken();
+        return res.status(200).json({ status: "success", user, token });
+      }
+    }
+
+    throw new Error("이메일 또는 비밀번호가 틀립니다!");
+  } catch (error) {
+    res.status(400).json({ status: "fail", error: error.message });
+  }
+};
+
+module.authController = authController;
--- models/User.js
+++ models/User.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const jwt = require("jsonwebtoken");
+require("dotenv");
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 const userSchema = Schema(
   {
     email: { type: String, required: true, unique: true },
@@ -17,6 +20,13 @@ userSchema.methods.toJSON = function () {
   delete obj.updateAt;
   delete obj.createAt;
   return obj;
+};
+
+userSchema.methods.generateToken = async function () {
+  const token = await jwt.sign({ _id: this.id }, JWT_SECRET_KEY, {
+    expiresIn: "1d",
+  });
+  return token;
 };
 
 const User = mongoose.model("User", userSchema);
