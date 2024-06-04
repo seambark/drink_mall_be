f64bc147b68d30b02b5c55aaa91a377a5b032813
Seam Park <run128@naver.com>
2024-06-05 02:43:40 +0900

    admin

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -43,4 +43,16 @@ authController.authenticate = async (req, res, next) => {
   }
 };
 
+authController.checkAdminPermission = async (req, res, next) => {
+  try {
+    const { userId } = req;
+    const user = await User.findById(userId);
+
+    if (user.level !== "admin") throw new Error("관리자가 아닙니다.");
+    next();
+  } catch (error) {
+    res.status(400).json({ status: "fail", error: error.message });
+  }
+};
+
 module.exports = authController;
