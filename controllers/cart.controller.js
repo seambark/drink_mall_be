7cbbb9e10118997fc0c3a312b1407e2144e4dcf4
Seam Park <run128@naver.com>
2024-06-09 00:56:48 +0900

    add cart

---
+++ controllers/cart.controller.js
@@ -0,0 +1,36 @@
+const Cart = require("../models/Cart");
+
+const cartController = {};
+
+cartController.addItemToCart = async (req, res) => {
+  try {
+    const { userId } = req;
+    const { productId, size, qty } = req.body;
+
+    let cart = await Cart.findOne({ userId });
+
+    if (!cart) {
+      cart = new Cart({ userId });
+      await cart.save();
+    }
+
+    const existItem = cart.items.find(
+      (item) => item.productId.equals(productId) && item.size === size
+    );
+
+    if (existItem) {
+      throw new Error("상품이 이미 카트에 담겨있습니다.");
+    }
+
+    cart.items = [...cart.items, { productId, size, qty }];
+    await cart.save();
+
+    res
+      .status(200)
+      .json({ status: "success", data: cart, cartItemQty: cart.items.length });
+  } catch (error) {
+    res.status(400).json({ status: "fail", error: error.message });
+  }
+};
+
+module.exports = cartController;
