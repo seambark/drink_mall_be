c800603a60a6cd7cf89499c3c909d0ee22ce6bf4
Seam Park <run128@naver.com>
2024-06-10 02:40:23 +0900

    cartPage update

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -50,4 +50,58 @@ cartController.getCart = async (req, res) => {
   }
 };
 
+cartController.deleteCartItem = async (req, res) => {
+  try {
+    const { userId } = req;
+    const { id } = req.params.id;
+    const cart = await Cart.findOne({ userId });
+    cart.items = cart.items.filter((item) => !item._id.equals(id));
+
+    await cart.save();
+    res.status(200).json({ status: "success", cartItemQty: cart.items.length });
+  } catch (error) {
+    res.status(400).json({ status: "fail", error: error.message });
+  }
+};
+
+cartController.editCartItem = async (req, res) => {
+  try {
+    const { userId } = req;
+    const { id } = req.params;
+    const { qty } = req.body;
+
+    const cart = await Cart.findOne({ userId }).populate({
+      path: "items",
+      populate: {
+        path: "productId",
+        mode: "Product",
+      },
+    });
+
+    if (!cart) throw new Error("카트에 상품이 없습니다.");
+    const index = cart.items.findIndex((item) => item._id.equals(id));
+
+    if (index === -1) throw new Error("상품을 찾을 수 없습니다.");
+    cart.items[index].qty = qty;
+    await cart.save();
+
+    res.status(200).json({ status: "success", data: cart.items });
+  } catch (error) {
+    res.status(400).json({ status: "fail", error: error.message });
+  }
+};
+
+cartController.getCartQty = async (req, res) => {
+  try {
+    const { userId } = req;
+    const cart = await Cart.findOne({ userId: userId });
+
+    if (!cart) throw new Error("카트내역이 존재하지 않습니다.");
+
+    res.status(200).json({ status: "success", data: cart.items.length });
+  } catch (error) {
+    res.status(400).json({ status: "fail", error: error.message });
+  }
+};
+
 module.exports = cartController;
--- routes/cart.api.js
+++ routes/cart.api.js
@@ -5,5 +5,12 @@ const cartController = require("../controllers/cart.controller");
 
 router.post("/", authController.authenticate, cartController.addItemToCart);
 router.get("/", authController.authenticate, cartController.getCart);
+router.delete(
+  "/:id",
+  authController.authenticate,
+  cartController.deleteCartItem
+);
+router.put("/:id", authController.authenticate, cartController.editCartItem);
+router.get("/qty", authController.authenticate, cartController.getCartQty);
 
 module.exports = router;
