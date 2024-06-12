ba6a6e2dded25fab8f5fd5580dbb88c2d8dc1b31
Seam Park <run128@naver.com>
2024-06-12 21:48:02 +0900

    orderList

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -31,10 +31,81 @@ orderController.createOrder = async (req, res) => {
     });
 
     await newOrder.save();
+
     res.status(200).json({ status: "success", orderNum: newOrder.orderNum });
   } catch (error) {
     res.status(400).json({ status: "fail", error: error.message });
   }
 };
 
+orderController.getOrder = async (req, res) => {
+  try {
+    const { userId } = req;
+    const { pageSize } = req.query;
+    let PAGE_SIZE = pageSize || 10;
+    const orderList = await Order.find({ userId: userId }).populate({
+      path: "items",
+      populate: {
+        path: "productId",
+        model: "Product",
+        select: "image name",
+      },
+    });
+    const totalItemNum = await Order.find({ userId: userId }).count();
+    const totalPageNum = Math.ceil(totalItemNum / PAGE_SIZE);
+
+    res.status(200).json({ status: "success", data: orderList, totalPageNum });
+  } catch (error) {
+    res.status(400).json({ status: "fail", error: error.message });
+  }
+};
+
+orderController.getOrderList = async (req, res) => {
+  try {
+    const { page, orderNum, pageSize } = req.query;
+    let PAGE_SIZE = pageSize || 10;
+    let cond = {};
+    if (orderNum) {
+      cond = {
+        orderNum: { $regex: orderNum, $options: "i" },
+      };
+    }
+
+    const orderList = await Order.find(cond)
+      .populate("userId")
+      .populate({
+        path: "items",
+        populate: {
+          path: "productId",
+          model: "Product",
+          select: "image name",
+        },
+      })
+      .skip((page - 1) * PAGE_SIZE)
+      .limit(PAGE_SIZE);
+
+    const totalItemNum = await Order.find(cond).count();
+    const totalPageNum = Math.ceil(totalItemNum / PAGE_SIZE);
+    res.status(200).json({ status: "success", data: orderList, totalPageNum });
+  } catch (error) {
+    res.status(400).json({ status: "fail", error: error.message });
+  }
+};
+
+orderController.updateOrder = async (req, res) => {
+  try {
+    const { id } = req;
+    const { status } = req.body;
+    const order = await Order.findByIdAndUpdate(
+      id,
+      { status: status },
+      { new: true }
+    );
+
+    if (!order) throw new Error("주문내역을 찾을 수 없습니다.");
+    res.status(200).json({ status: "success", data: order });
+  } catch (error) {
+    res.status(400).json({ status: "fail", error: error.message });
+  }
+};
 module.exports = orderController;
